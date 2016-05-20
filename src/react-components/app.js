bb9428c64b570e69834d3e32e581b19454a6e128
martin byrne <marb61a@gmail.com>
2016-05-20 10:12:34 +0000

    added initial app.js

---
+++ src/react-components/app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePage from './HomePage';
+import Navbar from './Navbar';
+import ProductStore from '../stores/ProductStore';
+import Actions from '../actions';
+
+class App extends React.component{
+    constructor(){
+        super();
+        Actions.initSession();
+    }
+    
+    static getStores() {
+        return [ProductStore];
+    }
+
+    render(){
+        return(
+            <section>
+            </section>
+        );
+    }
+}
+
