da9969d431589af2b1d3d2704986a1446ab69822
martin byrne <marb61a@gmail.com>
2016-05-23 10:03:29 +0000

    added navbar index

---
+++ src/react-components/Navbar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+          popupStatus: false
+        };
+    }
+    
+    render(){
+        return(
+            <section>
+                <section className="navbar">
+                
+                </section>
+            </section>    
+        );
+    }
+}
+
+export default Navbar;
