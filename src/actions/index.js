dc01f32086249e0206b18f06174753b7e15518c0
martin byrne <marb61a@gmail.com>
2016-05-23 09:57:52 +0000

    added index pages to alt, actions and homepage folders

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import alt from '../alt';
+import Firebase from 'firebase';
+import _ from 'lodash';
+
+class Actions {
+    
+}
+
+export default alt.createActions(Actions);
---
+++ src/react-components/Homepage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductList from '../Product/ProductList';
+import Firebase from 'firebase';
+import connectToStores from 'alt-utils/lib/connectToStores';
+import ProductStore from '../../stores/ProductStore';
+import Actions from '../../actions';
+
+class HomePage extends React.Component {
+    
+}
+
+export default HomePage;
