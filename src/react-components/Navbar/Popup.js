3362efc060bfd3c735c39bd2d590733f860dd13d
martin byrne <marb61a@gmail.com>
2016-05-23 10:13:19 +0000

    added popup to navbar

---
+++ src/react-components/Navbar/Popup.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Popup extends React.Component {
+    renderPopupContent(){
+        return(
+            <section className="popup">
+                <section className="popup-wrap">
+            
+                </section>
+                <section className="popup-content">
+            
+                </section>
+            </section>
+        );
+    }
+    
+    render(){
+        return(
+            <section>
+            
+            </section>    
+        );
+    }
+}
+
+export default Popup;
