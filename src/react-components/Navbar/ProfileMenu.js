38265f6ac92b515e775ed7ac662c6c338af0e6c5
martin byrne <marb61a@gmail.com>
2016-05-24 13:12:30 +0000

    added basic profilemenu file

---
+++ src/react-components/Navbar/ProfileMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Actions from '../../actions';
+
+class ProfileMenu extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+          showProfileNav: false
+        };
+    }
+    
+    render(){
+        return(
+            <section className="profile-menu">
+            {
+              
+            }
+            </section>
+        );
+    }
+    
+}
+
+export default ProfileMenu;
