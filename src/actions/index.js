c46fa1705b0b40de19463187bed0332a8f15e9e8
martin byrne <marb61a@gmail.com>
2016-05-25 10:03:20 +0000

    added initsession function to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,25 @@ import Firebase from 'firebase';
 import _ from 'lodash';
 
 class Actions {
-    
+    initSession(){
+        return(dispatch) => {
+            var firebaseRef = new Firebase('');
+            var authData = firebaseRef.getAuth();
+            var user;
+            
+            if(authData){
+                user = {
+                    id: authData.facebook.id,
+                    name: authData.facebook.displayName,
+                    avatar: authData.facebook.profileImageURL
+                };                
+            } else{
+                user = null;                
+            }
+            
+            setTimeout(() => dispatch(user));
+        };
+    }
 }
 
 export default alt.createActions(Actions);
