97f84cfca2c4a18b121085e7d323aad78c6976fb
martin byrne <marb61a@gmail.com>
2016-05-27 13:57:04 +0000

    updated homepage index file

--- src/react-components/Homepage/index.js
+++ src/react-components/Homepage/index.js
@@ -6,7 +6,35 @@ import ProductStore from '../../stores/ProductStore';
 import Actions from '../../actions';
 
 class HomePage extends React.Component {
+    constructor() {
+        super();
+        Actions.getProducts();
+    }
     
+    static getStores() {
+        return [ProductStore];
+    }
+    
+    static getPropsFromStores() {
+        return ProductStore.getState();
+    }
+    
+    render(){
+        return(
+            <section>
+                <header>
+                    <img src="/img/banner.jpeg" width="100%" />
+                </header>
+                <section>
+                    <section className="container">
+                        {
+                            
+                        }
+                    </section>
+                </section>
+            </section>
+        );
+    }
 }
 
 export default HomePage;
