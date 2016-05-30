fe6eda53a995d72ecd5511f9f2c862b104b351c2
martin byrne <marb61a@gmail.com>
2016-05-30 10:40:02 +0000

    updated postpopup

--- src/react-components/Navbar/PostPopup.js
+++ src/react-components/Navbar/PostPopup.js
@@ -4,18 +4,36 @@ import Actions from '../../actions';
 
 class PostPopup extends React.Component {
     handlePost = () => {
-        
+      var newProduct = {
+        name: this.refs.name.value,
+        link: this.refs.link.value,
+        description: this.refs.description.value,
+        media: this.refs.media.value,
+        upvote: 0,
+        maker: {
+          name: this.props.user.name,
+          avatar: this.props.user.avatar
+        }
+      };
+      
+      Actions.addProduct(newProduct);
     }
     
     render(){
         return(
-          <Popup>
+          <Popup  {...this.props} style="post-popup">
             <header className="post-header">Post a new product</header>
             <section>
-            
+              <table>
+                <tbody>
+                
+                </tbody>
+              </table>
             </section>
             <footer className="post-footer">
-            
+              <button onClick={this.handlePost} className="post-btn">
+                Hunt It!
+              </button>
             </footer>
           </Popup>  
         );
