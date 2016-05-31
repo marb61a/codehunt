227b1bc38ccc82eedd483f0630ea9e95980e7c7e
martin byrne <marb61a@gmail.com>
2016-05-31 10:28:41 +0000

    updated profilemenu

--- src/react-components/Navbar/ProfileMenu.js
+++ src/react-components/Navbar/ProfileMenu.js
@@ -9,6 +9,27 @@ class ProfileMenu extends React.Component {
         };
     }
     
+    handleClick = () => {
+        if(this.state.showProfileNav){
+            this.setState({
+                showProfileNav : false
+            });
+        } else {
+            this.setState({
+                showProfileNav: true
+            });
+        }
+    }
+    
+    renderProfileNav(){
+        return(
+            <nav className="profile-nav" ref="profileNav">
+                <a href="#">My Profile</a>
+                <a href="#" onClick={this.handleLogout}>Logout</a>
+            </nav>    
+        );
+    }
+    
     render(){
         return(
             <section className="profile-menu">
